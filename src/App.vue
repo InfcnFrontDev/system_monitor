22340618c6efa7c8d0e0784d1192a3e6dc40d92c
yangkk <yangkk@infcn.com.cn>
2016-09-21 17:10:57 +0800

    add vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-header></v-header>
+    <v-content></v-content>
+    <v-footer></v-footer>
+</template>
+<script>
+    
+    import VHeader from '../../common/components/VHeader.vue'
+    import VFooter from '../../common/components/VFooter.vue'
+    import VContent from './components/VContent.vue'
+
+    export default{
+        components: {
+            VHeader, VFooter, VContent
+        },
+        ready() {
+            
+        }
+    }
+</script>
---
+++ src/components/VContent.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="banner">
+        <div class="body">
+            <div class="line">
+                <div v-for="item in items" :class="{'blue':item.selected}">
+                    <a href="details.html#!/{{item.id}}" @mouseover="mouseover(item)" @mouseout="mouseout(item)">
+                        <img :src="item.src">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+    export default{
+        ready() {
+        },
+        data(){
+            return {
+                items: [
+                    {id: 'pdf2doc', src: 'static/images/pdf2doc.png', selected: false},
+                    {id: 'pdf2ppt', src: 'static/images/pdf2ppt.png', selected: false},
+                    {id: 'pdf2xls', src: 'static/images/pdf2xls.png', selected: false},
+                    {id: 'pdf2html', src: 'static/images/pdf2html.png', selected: false},
+                    {id: 'pdf2txt', src: 'static/images/pdf2txt.png', selected: false},
+                    {id: 'pdf2img', src: 'static/images/pdf2img.png', selected: false},
+                    {id: 'doc2pdf', src: 'static/images/doc2pdf.png', selected: false},
+                    {id: 'txt2pdf', src: 'static/images/txt2pdf.png', selected: false},
+                    {id: 'ppt2pdf', src: 'static/images/ppt2pdf.png', selected: false},
+                    {id: 'html2pdf', src: 'static/images/html2pdf.png', selected: false},
+                    {id: 'pdf2rtf', src: 'static/images/pdf2rtf.png', selected: false},
+                    {id: 'pdf2xml', src: 'static/images/pdf2xml.png', selected: false},
+                    {id: 'pdfjiemi', src: 'static/images/pdfjiemi.png', selected: false},
+                    {id: 'pdfjiami', src: 'static/images/pdfjiami.png', selected: false},
+                    {id: 'pdfhebing', src: 'static/images/pdfhebing.png', selected: false},
+                    {id: 'pdffenge', src: 'static/images/pdffenge.png', selected: false},
+                    {id: 'pdfhuoqu', src: 'static/images/pdfhuoqu.png', selected: false},
+                    {id: 'pdfshibie', src: 'static/images/pdfshibie.png', selected: false}
+                ]
+            }
+        },
+        methods: {
+            mouseover(item){
+                item.src = 'static/images/'+ item.id +'_.png';
+                item.selected = true;
+            },
+            mouseout(item){
+                item.src = 'static/images/'+ item.id +'.png';
+                item.selected = false;
+            }
+        }
+
+    }
+</script>
