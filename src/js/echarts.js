730de5b38cbbe83d0d3464d95948622d79a12270
yangkk <yangkk@infcn.com.cn>
2016-09-21 16:28:21 +0800

    echarts.js

--- src/js/echarts.js
+++ src/js/echarts.js
@@ -1,7 +1,6 @@
 var ECharts = {};
 
-var $ajax = function (url, success, error) {
-
+ECharts.ajax = function (url, success, error) {
     $.ajax({
         url: url,
         cache: false,
@@ -15,115 +14,32 @@ var $ajax = function (url, success, error) {
     });
 };
 
-// Line
-ECharts.Line = function (options) {
+ECharts.baseConfig = {
+    element: 'id',
+    dataUrl: '/api/bardata',
+    dataMap: function (data) {
+        return data;
+    },
+    interval: 3000,
+    legend: false
+};
 
-    options = $.extend({
-        element: 'id'
-    }, options);
+// Line
+ECharts.Line = function (config) {
+
+    config = $.extend({}, ECharts.baseConfig, {}, config);
 
     // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById(options.element));
+    var myChart = echarts.init(document.getElementById(config.element));
 
     // 指定图表的配置项和数据
     var option = {
-        tooltip: {
-            trigger: 'axis'
-        },
-        legend: {
-            data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
-        },
-        grid: {
-            top: '15%',
-            left: '3%',
-            right: '4%',
-            bottom: '3%',
-            containLabel: true
-        },
-        xAxis: [
-            {
-                type: 'category',
-                boundaryGap: false,
-                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-            }
-        ],
-        yAxis: [
-            {
-                type: 'value'
-            }
-        ],
-        series: [
-            {
-                name: '邮件营销',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {normal: {}},
-                data: [120, 132, 101, 134, 90, 230, 210]
-            },
-            {
-                name: '联盟广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {normal: {}},
-                data: [220, 182, 191, 234, 290, 330, 310]
-            },
-            {
-                name: '视频广告',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {normal: {}},
-                data: [150, 232, 201, 154, 190, 330, 410]
-            },
-            {
-                name: '直接访问',
-                type: 'line',
-                stack: '总量',
-                areaStyle: {normal: {}},
-                data: [320, 332, 301, 334, 390, 330, 320]
-            },
-            {
-                name: '搜索引擎',
-                type: 'line',
-                stack: '总量',
-                label: {
-                    normal: {
-                        show: true,
-                        position: 'top'
-                    }
-                },
-                areaStyle: {normal: {}},
-                data: [820, 932, 901, 934, 1290, 1330, 1320]
-            }
-        ]
-    };
-
-
-    // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
-
-    return myChart;
-}
-
-// Bar
-ECharts.Bar = function (options) {
-
-    options = $.extend({
-        element: 'id',
-        dataUrl: '/api/bardata',
-        legend: false
-    }, options);
-
-    // 基于准备好的dom，初始化echarts实例
-    var myChart = echarts.init(document.getElementById(options.element));
-
-    // 指定图表的配置项和数据
-    var op = {
         tooltip: {},
         legend: {
-            show: options.legend,
+            show: config.legend,
             data: []
         },
-        grid: {top: options.legend?'15%':'3%', left: '3%', right: '3%', bottom: '3%', containLabel: true},
+        grid: {top: (config.legend ? '15%' : '3%'), left: '3%', right: '3%', bottom: '3%', containLabel: true},
         xAxis: {
             data: []
         },
@@ -131,29 +47,78 @@ ECharts.Bar = function (options) {
         series: []
     };
 
-    myChart.setOption(op);
 
-    function fetchData(cb) {
-        $ajax('/api/bardata', function (data) {
-            cb(data);
+    myChart.setOption(option);
+
+    function fetchData() {
+        ECharts.ajax('/api/bardata', function (data) {
+            myChart.hideLoading();
+            render(config.dataMap(data));
         });
     }
+
     function render(data) {
-        op.xAxis.data = data.category;
-        op.series = data.series;
-        for(var i in op.series){
-            op.series[i].type = 'bar';
-            op.legend.data.push(op.series[i].name);
+        option.xAxis.data = data.category;
+        option.series = data.series;
+        for (var i in option.series) {
+            option.series[i].type = 'line';
+            option.legend.data.push(option.series[i].name);
         }
-        myChart.setOption(op);
+        myChart.setOption(option);
     }
 
     myChart.showLoading();
+    fetchData();
+    setInterval(fetchData, config.interval); //指定n秒刷新一次
 
-    fetchData(function (data) {
-        myChart.hideLoading();
-        render(data);
-    });
+    return myChart;
+}
+
+// Bar
+ECharts.Bar = function (config) {
+
+    config = $.extend({}, ECharts.baseConfig, {}, config);
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById(config.element));
+
+    // 指定图表的配置项和数据
+    var option = {
+        tooltip: {},
+        legend: {
+            show: config.legend,
+            data: []
+        },
+        grid: {top: (config.legend ? '15%' : '3%'), left: '3%', right: '3%', bottom: '3%', containLabel: true},
+        xAxis: {
+            data: []
+        },
+        yAxis: {},
+        series: []
+    };
+
+    myChart.setOption(option);
+
+    function fetchData() {
+        ECharts.ajax('/api/bardata', function (data) {
+            myChart.hideLoading();
+            render(config.dataMap(data));
+        });
+    }
+
+    function render(data) {
+        option.xAxis.data = data.category;
+        option.series = data.series;
+        for (var i in option.series) {
+            option.series[i].type = 'bar';
+            option.legend.data.push(option.series[i].name);
+        }
+        myChart.setOption(option);
+    }
+
+    myChart.showLoading();
+    fetchData();
+    setInterval(fetchData, config.interval); //指定n秒刷新一次
 
     return myChart;
 }
--- src/js/monitor.js
+++ src/js/monitor.js
@@ -1,8 +1,214 @@
+var Monitor = {};
 
-ECharts.Bar({
-    element: 'bar-chart'
-});
+/**
+ * config
+ */
+Monitor.config = {
+    apiPath: 'http://192.168.10.102:8080/ifcmonitor/',
+    interval: 3000,
+    legend: false
+};
 
-ECharts.Line({
-    element: 'line-chart'
-});
+/**
+ * ajax
+ */
+Monitor.ajax = function (url, success, error) {
+    $.ajax({
+        url: url,
+        cache: false,
+        dataType: 'json',
+        success: function (data, textStatus, jqXHR) {
+            success && success(data, textStatus, jqXHR);
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            error && error(XMLHttpRequest, textStatus, errorThrown);
+        }
+    });
+};
+
+/**
+ * ajax
+ */
+Monitor.toHHmm = function (date) {
+    return date.substring(8, 10) + ':' + date.substring(10, 12);
+};
+
+/**
+ * 存储使用情况
+ */
+Monitor.StorageUsage = function (config) {
+    config = $.extend({}, Monitor.config, {}, config);
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById(config.element));
+
+    // 指定图表的配置项和数据
+    var option = {
+        tooltip: {
+            trigger: 'item',
+            formatter: "{b}: {c} ({d}%)"
+        },
+        series: [
+            {
+                type: 'pie',
+                selectedMode: 'single',
+                radius: [0, '75%'],
+                data: []
+            }
+        ]
+    };
+    myChart.setOption(option);
+
+    function fetchData() {
+        Monitor.ajax(config.apiPath + 'IFCMonitorServlet?type=fs', function (result) {
+            myChart.hideLoading();
+            console.log('StorageUsage', result)
+            option.series[0].data = mapData(result);
+            myChart.setOption(option);
+        });
+    }
+
+    function mapData(result) {
+
+        var useds = 0, frees = 0;
+        $(result.ifcFileSystems).each(function () {
+            useds += this.used;
+            frees += this.free;
+        });
+
+        return [
+            {value: useds, name: '已用存储'},
+            {value: frees, name: '可用存储', selected: true}
+        ];
+    }
+
+    myChart.showLoading();
+    fetchData();
+    //setInterval(fetchData, config.interval); //指定n秒刷新一次
+};
+
+/**
+ * CPU使用情况
+ */
+Monitor.CpuUsage = function (config) {
+    config = $.extend({}, Monitor.config, {
+        interval: 1000
+    }, config);
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById(config.element));
+
+    // 指定图表的配置项和数据
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        grid: {
+            top: '15%', left: '5%', right: '5%', bottom: '5%', containLabel: true
+        },
+        xAxis: [{
+            type: 'category',
+            boundaryGap: false,
+            data: []
+        }],
+        yAxis: [{
+            name: '利用率（%）',
+            type: 'value',
+            max: 100
+        }],
+        series: []
+    };
+
+    myChart.setOption(option);
+
+    function fetchData() {
+        Monitor.ajax(config.apiPath + 'IFCMonitorServlet?monitorDate=', function (result) {
+            myChart.hideLoading();
+
+            var syssData = [], usersData = [];
+            $(result).each(function () {
+                option.xAxis[0].data.push(Monitor.toHHmm(this.date));
+
+                var syss = 0, users = 0;
+                $(this.ifcCpus).each(function () {
+                    syss += this.sys;
+                    users += this.user;
+                });
+                syssData.push((syss * 100).toFixed(2));
+                usersData.push((users * 100).toFixed(2));
+            });
+
+            option.series = [{
+                name: '用户', type: 'line', stack: '总量', areaStyle: {normal: {}}, data: usersData
+            }, {
+                name: '系统', type: 'line', stack: '总量', areaStyle: {normal: {}}, data: syssData
+            }];
+
+            myChart.setOption(option);
+        });
+    }
+
+    myChart.showLoading();
+    fetchData();
+};
+
+/**
+ * 内存使用情况
+ */
+Monitor.MemoryUsage = function (config) {
+    config = $.extend({}, Monitor.config, {
+        interval: 1000
+    }, config);
+
+    // 基于准备好的dom，初始化echarts实例
+    var myChart = echarts.init(document.getElementById(config.element));
+
+    // 指定图表的配置项和数据
+    var option = {
+        tooltip: {
+            trigger: 'axis'
+        },
+        grid: {
+            top: '15%', left: '5%', right: '5%', bottom: '5%', containLabel: true
+        },
+        xAxis: [{
+            type: 'category',
+            boundaryGap: false,
+            data: []
+        }],
+        yAxis: [{
+            name: '容量（GB）',
+            type: 'value'
+        }],
+        series: []
+    };
+
+    myChart.setOption(option);
+
+    function fetchData() {
+        Monitor.ajax(config.apiPath + 'IFCMonitorServlet?monitorDate=', function (result) {
+            myChart.hideLoading();
+
+            var usedData = [], freeData = [];
+            $(result).each(function () {
+                option.xAxis[0].data.push(Monitor.toHHmm(this.date));
+                var used = (this.ifcMem.used /1024/1024/1024).toFixed(3);
+                var free = (this.ifcMem.free /1024/1024/1024).toFixed(3);
+                usedData.push(used);
+                freeData.push(free);
+                option.yAxis[0].max = (parseFloat(used) + parseFloat(free)).toFixed(0);
+            });
+
+            option.series = [{
+                name: '已用', type: 'line', stack: '总量', areaStyle: {normal: {}}, data: usedData
+            }, {
+                name: '可用', type: 'line', stack: '总量', areaStyle: {normal: {}}, data: freeData
+            }];
+
+            myChart.setOption(option);
+        });
+    }
+
+    myChart.showLoading();
+    fetchData();
+};
