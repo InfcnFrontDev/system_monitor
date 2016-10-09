cbfd67e1446c08549403616ec5dc38b163ee3cee
yangkk <yangkk@infcn.com.cn>
2016-10-09 18:27:41 +0800

    disk、jvmclass、net

--- src/components/WidgetDiskUsage.vue
+++ src/components/WidgetDiskUsage.vue
@@ -39,7 +39,7 @@
                 },
                 legend: {
                     top: 14,
-                    data:['写入速度', '读取速度']
+                    data: ['写入速度', '读取速度']
                 },
                 xAxis: [{
                     type: 'category',
@@ -58,109 +58,218 @@
             });
         },
         methods: {
-            periodChange(monitorDate, interval){
-                if (monitorDate) {
-                    this.intervalStatistics(monitorDate, interval);
-                } else {
-                    this.realtimeMonitor();
-                }
-            },
-            intervalStatistics(monitorDate, interval){
-                this.$refs.chart.showLoading();
-
+            // 时间段改变时
+            periodChange(monitorDate, interval, isAllDay){
                 // 清除实时监控的定时器
                 if (this.timer != null)
                     clearInterval(this.timer);
 
-                this.intervalFetchData(monitorDate, interval);
-            },
-            intervalFetchData(monitorDate, interval){
-                let $this = this;
-                Monitor.getFileSystems(monitorDate, interval).then(function (result) {
-                    $this.$refs.chart.hideLoading();
-                    $this.intervalRender(result);
-                });
-            },
-            intervalRender(result) {
-                let xAxisData = [], data1 = [], data2 = [], temp1 = [], temp2 = [];
+                this.monitorDate = monitorDate;
+                this.interval = interval;
+                this.isAllDay = isAllDay;
+                this.intervalTime = monitorDate ? 1000 * 60 * interval : Config.realtimeIntervalTime;
 
-                $(result).each(function (i) {
-                    xAxisData.push(Tools.dateToHHmm(this.date));
-
-                    var diskReadBytes = 0, diskWriteBytes = 0;
-                    $(this.ifcFileSystems).each(function () {
-                        diskReadBytes += this.diskReadBytes;
-                        diskWriteBytes += this.diskWriteBytes;
-                    });
-                    temp1.push(diskReadBytes);
-                    temp2.push(diskWriteBytes);
-
-                    let d1 = 0, d2 = 0;
-                    if (i > 0) {
-                        d1 = temp1[i] - temp1[i - 1];
-                        d2 = temp2[i] - temp2[i - 1];
-                    }
-                    data1.push(Tools.byteToKB(d1).toFixed(2));
-                    data2.push(Tools.byteToKB(d2).toFixed(2));
-                });
-
-                this.$refs.chart.setOption({
-                    xAxis: [{data: xAxisData}],
-                    series: [{data: data1}, {data: data2}]
-                });
-            },
-            realtimeMonitor() {
                 this.$refs.chart.showLoading();
 
+                // 实时监控初始化
+                if (!this.monitorDate) {
+                    this.realtimeInit();
+                }
+
+                let $this = this;
+                setTimeout(function () {// 解决先执行一次的问题
+                    $this.fetchData();
+                    $this.timer = setInterval($this.fetchData, $this.intervalTime); // 定时执行数据抓取
+                }, 0);
+            },
+            // 抓取数据
+            fetchData(){
+                let $this = this;
+                Monitor.getFileSystems(this.monitorDate, this.interval).then(function (result) {
+                    $this.fetchSuccess(result);
+                }, function (error) {
+                    $this.fetchError(error);
+                });
+            },
+            // 获取数据成功时处理
+            fetchSuccess(result){
+                this.$refs.chart.hideLoading();
+
+                if (this.monitorDate) {
+                    if (result instanceof Array)
+                        if (this.isAllDay) {
+                            this.allDayRender(result);
+                        } else {
+                            this.intervalRender(result);
+                        }
+                } else {
+                    if (!(result instanceof Array))
+                        this.realtimeRender(result);
+                }
+            },
+            // 获取数据出错时处理
+            fetchError(error){
+                this.$refs.chart.hideLoading();
+
+                if (this.monitorDate) {
+                    if (this.isAllDay) {
+                        this.allDayInit();
+                    } else {
+                        this.realtimeInit();
+                    }
+                } else {
+                    this.realtimeInit();
+                }
+            },
+            // 实时监控初始化
+            realtimeInit() {
                 let xAxisData = [], data1 = [], data2 = [];
-                xAxisData.length = 61;
-                data1.length = 61;
-                data2.length = 61;
+                xAxisData.length = Config.realtimeLen;
+                data1.length = Config.realtimeLen;
+                data2.length = Config.realtimeLen;
+
+                this.diskReadBytes = undefined;
+                this.diskWriteBytes = undefined;
 
                 this.$refs.chart.setOption({
                     xAxis: [{data: xAxisData}],
+                    yAxis: [{max: this.yAxisMax}],
                     series: [{data: data1}, {data: data2}]
                 });
-
-                this.timer = setInterval(this.realtimeFetchData, 1000);
             },
-            realtimeFetchData() {
+            // 实时监控数据渲染
+            realtimeRender(result) {
+                let option = this.$refs.chart.getOption(),
+                        xAxisData = option.xAxis[0].data,
+                        data1 = option.series[0].data,
+                        data2 = option.series[1].data,
+                        yAxisMax = option.yAxis[0].max;
+
+                let itemData = this.toItemData(result);
+
+                let date = new Date();
+                xAxisData.shift();
+                xAxisData.push(Tools.dateFormat(date, Tools.HHmmss_));
+
+                data1.shift();
+                data1.push(itemData.data1);
+                data2.shift();
+                data2.push(itemData.data2);
+
+                // y轴max值最小为100，否则自动
+                if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                    yAxisMax = 'auto';
+                }
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                })
+            },
+            // 区间展示数据渲染
+            intervalRender(result) {
+                let xAxisData = [], data1 = [], data2 = [], yAxisMax = this.yAxisMax;
+
+                this.diskReadBytes = undefined;
+                this.diskWriteBytes = undefined;
+
                 let $this = this;
-                Monitor.getFileSystems().then(function (result) {
-                    $this.$refs.chart.hideLoading();
-                    $this.realtimeRender(result)
+                $(result).each(function () {
+                    let itemData = $this.toItemData(this);
+
+                    xAxisData.push(itemData.xAxisData);
+                    data1.push(itemData.data1);
+                    data2.push(itemData.data2);
+
+                    // y轴max值最小为100，否则自动
+                    if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                        yAxisMax = 'auto';
+                    }
+                });
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
                 });
             },
-            realtimeRender(result) {
-                let option = this.$refs.chart.getOption();
-                var xAxisData = option.xAxis[0].data, data1 = option.series[0].data, data2 = option.series[1].data;
+            // 全天展示初始化
+            allDayInit(){
+                let len = 60 * 24 / this.interval,
+                        xAxisData = [], data1 = [], data2 = [];
 
-                xAxisData.shift();
-                xAxisData.push(Tools.dateFormat(new Date(), Tools.HHmmss_));
+                xAxisData.length = len;
+                data1.length = len;
+                data2.length = len;
+
+                for (let i = 0; i < xAxisData.length; i++) {
+                    xAxisData[i] = Tools.numberToTime(i, this.interval);
+                    data1[i] = '-';
+                    data2[i] = '-';
+                }
+
+                this.diskReadBytes = undefined;
+                this.diskWriteBytes = undefined;
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: this.yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                });
+            },
+            // 全天展示数据渲染
+            allDayRender(result) {
+                this.allDayInit();
+
+                let option = this.$refs.chart.getOption(),
+                        data1 = option.series[0].data,
+                        data2 = option.series[1].data,
+                        yAxisMax = this.yAxisMax,
+                        interval = this.interval;
+
+                let $this = this;
+                $(result).each(function () {
+                    let itemData = $this.toItemData(this),
+                            num = Tools.timeToNumber(itemData.xAxisData, interval);
+
+                    data1[num] = itemData.data1;
+                    data2[num] = itemData.data2;
+
+                    // y轴max值最小为100，否则自动
+                    if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                        yAxisMax = 'auto';
+                    }
+                });
+
+                this.$refs.chart.setOption({
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                });
+            },
+            // 数据转换
+            toItemData(item) {
+                let xAxisData = Tools.dateToHHmm(item.date), data1 = 0, data2 = 0, yAxisMax = 0;
 
                 var diskReadBytes = 0, diskWriteBytes = 0;
-                $(result.ifcFileSystems).each(function () {
+                $(item.ifcFileSystems).each(function () {
                     diskReadBytes += this.diskReadBytes;
                     diskWriteBytes += this.diskWriteBytes;
                 });
 
                 let d1 = 0, d2 = 0;
-                if(this.diskReadBytes != undefined && this.diskWriteBytes != undefined){
-                    d1 =  diskReadBytes - this.diskReadBytes;
-                    d2 =  diskWriteBytes - this.diskWriteBytes;
+                if (this.diskReadBytes != undefined && this.diskWriteBytes != undefined) {
+                    d1 = diskReadBytes - this.diskReadBytes;
+                    d2 = diskWriteBytes - this.diskWriteBytes;
                 }
+                data1 = parseFloat(Tools.byteToKB(d1).toFixed(2));
+                data2 = parseFloat(Tools.byteToKB(d2).toFixed(2));
+                yAxisMax = data1 > data2 ? data1 : data2;
+
                 this.diskReadBytes = diskReadBytes;
                 this.diskWriteBytes = diskWriteBytes;
 
-                data1.shift();
-                data1.push(Tools.byteToKB(d1).toFixed(2));
-                data2.shift();
-                data2.push(Tools.byteToKB(d2).toFixed(2));
-
-                this.$refs.chart.setOption({
-                    xAxis: [{data: xAxisData}],
-                    series: [{data: data1}, {data: data2}]
-                })
+                return {xAxisData, yAxisMax, data1, data2}
             }
         }
     }
--- src/components/WidgetNetThroughput.vue
+++ src/components/WidgetNetThroughput.vue
@@ -42,7 +42,7 @@
                 },
                 legend: {
                     top: 14,
-                    data:['发送', '接收']
+                    data: ['发送', '接收']
                 },
                 xAxis: [{
                     type: 'category',
@@ -54,117 +54,226 @@
                     type: 'value'
                 }],
                 series: [{
-                    name: '发送', type: 'line', data: [],formatter:'{data}%'
+                    name: '发送', type: 'line', data: [], formatter: '{data}%'
                 }, {
-                    name: '接收', type: 'line', data: [],formatter:'{data}%'
+                    name: '接收', type: 'line', data: [], formatter: '{data}%'
                 }]
             });
         },
         methods: {
-            periodChange(monitorDate, interval){
-                if (monitorDate) {
-                    this.intervalStatistics(monitorDate, interval);
-                } else {
-                    this.realtimeMonitor();
-                }
-            },
-            intervalStatistics(monitorDate, interval){
-                this.$refs.chart.showLoading();
-
+            // 时间段改变时
+            periodChange(monitorDate, interval, isAllDay){
                 // 清除实时监控的定时器
                 if (this.timer != null)
                     clearInterval(this.timer);
 
-                this.intervalFetchData(monitorDate, interval);
-            },
-            intervalFetchData(monitorDate, interval){
-                let $this = this;
-                Monitor.getNets(monitorDate, interval).then(function (result) {
-                    $this.$refs.chart.hideLoading();
-                    $this.intervalRender(result);
-                });
-            },
-            intervalRender(result) {
-                let xAxisData = [], data1 = [], data2 = [];
+                this.monitorDate = monitorDate;
+                this.interval = interval;
+                this.isAllDay = isAllDay;
+                this.intervalTime = monitorDate ? 1000 * 60 * interval : Config.realtimeIntervalTime;
 
-                let data10 = [], data20 = [];
-                $(result).each(function (i) {
-                    xAxisData.push(Tools.dateToHHmm(this.date));
-
-                    var rxBytes = 0, txBytes = 0;
-                    $(this.ifcNets).each(function () {
-                        rxBytes += this.rxBytes;
-                        txBytes += this.txBytes;
-                    });
-                    data10.push(rxBytes);
-                    data20.push(txBytes);
-
-                    let d1 = 0, d2 = 0;
-                    if (i > 0) {
-                        d1 = data10[i] - data10[i - 1];
-                        d2 = data20[i] - data20[i - 1];
-                    }
-                    data1.push(Tools.byteToMB(d1).toFixed(2));
-                    data2.push(Tools.byteToMB(d2).toFixed(2));
-                });
-
-                this.$refs.chart.setOption({
-                    xAxis: [{data: xAxisData}],
-                    series: [{data: data1}, {data: data2}]
-                });
-            },
-            realtimeMonitor() {
                 this.$refs.chart.showLoading();
 
+                // 实时监控初始化
+                if (!this.monitorDate) {
+                    this.realtimeInit();
+                }
+
+                let $this = this;
+                setTimeout(function () {// 解决先执行一次的问题
+                    $this.fetchData();
+                    $this.timer = setInterval($this.fetchData, $this.intervalTime); // 定时执行数据抓取
+                }, 0);
+            },
+            // 抓取数据
+            fetchData(){
+                let $this = this;
+                Monitor.getNets(this.monitorDate, this.interval).then(function (result) {
+                    $this.fetchSuccess(result);
+                }, function (error) {
+                    $this.fetchError(error);
+                });
+            },
+            // 获取数据成功时处理
+            fetchSuccess(result){
+                this.$refs.chart.hideLoading();
+
+                if (this.monitorDate) {
+                    if (result instanceof Array)
+                        if (this.isAllDay) {
+                            this.allDayRender(result);
+                        } else {
+                            this.intervalRender(result);
+                        }
+                } else {
+                    if (!(result instanceof Array))
+                        this.realtimeRender(result);
+                }
+            },
+            // 获取数据出错时处理
+            fetchError(error){
+                this.$refs.chart.hideLoading();
+
+                if (this.monitorDate) {
+                    if (this.isAllDay) {
+                        this.allDayInit();
+                    } else {
+                        this.realtimeInit();
+                    }
+                } else {
+                    this.realtimeInit();
+                }
+            },
+            // 实时监控初始化
+            realtimeInit() {
                 let xAxisData = [], data1 = [], data2 = [];
-                xAxisData.length = 61;
-                data1.length = 61;
-                data2.length = 61;
+                xAxisData.length = Config.realtimeLen;
+                data1.length = Config.realtimeLen;
+                data2.length = Config.realtimeLen;
+
+                this.rxBytes = undefined;
+                this.txBytes = undefined;
 
                 this.$refs.chart.setOption({
                     xAxis: [{data: xAxisData}],
+                    yAxis: [{max: this.yAxisMax}],
                     series: [{data: data1}, {data: data2}]
                 });
-
-                this.timer = setInterval(this.realtimeFetchData, 1000);
             },
-            realtimeFetchData() {
-                let $this = this;
-                Monitor.getNets().then(function (result) {
-                    $this.$refs.chart.hideLoading();
-                    $this.realtimeRender(result)
-                });
-            },
+            // 实时监控数据渲染
             realtimeRender(result) {
-                let option = this.$refs.chart.getOption();
-                var xAxisData = option.xAxis[0].data, data1 = option.series[0].data, data2 = option.series[1].data;
+                let option = this.$refs.chart.getOption(),
+                        xAxisData = option.xAxis[0].data,
+                        data1 = option.series[0].data,
+                        data2 = option.series[1].data,
+                        yAxisMax = option.yAxis[0].max;
+
+                let itemData = this.toItemData(result);
+
                 let date = new Date();
                 xAxisData.shift();
                 xAxisData.push(Tools.dateFormat(date, Tools.HHmmss_));
 
+                data1.shift();
+                data1.push(itemData.data1);
+                data2.shift();
+                data2.push(itemData.data2);
+
+                // y轴max值最小为100，否则自动
+                if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                    yAxisMax = 'auto';
+                }
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                })
+            },
+            // 区间展示数据渲染
+            intervalRender(result) {
+                let xAxisData = [], data1 = [], data2 = [], yAxisMax = this.yAxisMax;
+
+                this.rxBytes = undefined;
+                this.txBytes = undefined;
+
+                let $this = this;
+                $(result).each(function () {
+                    let itemData = $this.toItemData(this);
+
+                    xAxisData.push(itemData.xAxisData);
+                    data1.push(itemData.data1);
+                    data2.push(itemData.data2);
+
+                    // y轴max值最小为100，否则自动
+                    if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                        yAxisMax = 'auto';
+                    }
+                });
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                });
+            },
+            // 全天展示初始化
+            allDayInit(){
+                let len = 60 * 24 / this.interval,
+                        xAxisData = [], data1 = [], data2 = [];
+
+                xAxisData.length = len;
+                data1.length = len;
+                data2.length = len;
+
+                for (let i = 0; i < xAxisData.length; i++) {
+                    xAxisData[i] = Tools.numberToTime(i, this.interval);
+                    data1[i] = '-';
+                    data2[i] = '-';
+                }
+
+                this.rxBytes = undefined;
+                this.txBytes = undefined;
+
+                this.$refs.chart.setOption({
+                    xAxis: [{data: xAxisData}],
+                    yAxis: [{max: this.yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                });
+            },
+            // 全天展示数据渲染
+            allDayRender(result) {
+                this.allDayInit();
+
+                let option = this.$refs.chart.getOption(),
+                        data1 = option.series[0].data,
+                        data2 = option.series[1].data,
+                        yAxisMax = this.yAxisMax,
+                        interval = this.interval;
+
+                let $this = this;
+                $(result).each(function () {
+                    let itemData = $this.toItemData(this),
+                            num = Tools.timeToNumber(itemData.xAxisData, interval);
+
+                    data1[num] = itemData.data1;
+                    data2[num] = itemData.data2;
+
+                    // y轴max值最小为100，否则自动
+                    if (yAxisMax != 'auto' && itemData.yAxisMax > yAxisMax) {
+                        yAxisMax = 'auto';
+                    }
+                });
+
+                this.$refs.chart.setOption({
+                    yAxis: [{max: yAxisMax}],
+                    series: [{data: data1}, {data: data2}]
+                });
+            },
+            // 数据转换
+            toItemData(item) {
+                let xAxisData = Tools.dateToHHmm(item.date), data1 = 0, data2 = 0, yAxisMax = 0;
+
                 var rxBytes = 0, txBytes = 0;
-                $(result.ifcNets).each(function () {
+                $(item.ifcNets).each(function () {
                     rxBytes += this.rxBytes;
                     txBytes += this.txBytes;
                 });
 
                 let d1 = 0, d2 = 0;
-                if(this.rxBytes0 != undefined && this.txBytes0 != undefined){
-                    d1 =  rxBytes - this.rxBytes0;
-                    d2 =  txBytes - this.txBytes0;
+                if (this.rxBytes != undefined && this.txBytes != undefined) {
+                    d1 = rxBytes - this.rxBytes;
+                    d2 = txBytes - this.txBytes;
                 }
-                this.rxBytes0 = rxBytes;
-                this.txBytes0 = txBytes;
 
-                data1.shift();
-                data1.push(Tools.byteToMB(d1).toFixed(2));
-                data2.shift();
-                data2.push(Tools.byteToMB(d2).toFixed(2));
+                data1 = parseFloat(Tools.byteToMB(d1).toFixed(2));
+                data2 = parseFloat(Tools.byteToMB(d2).toFixed(2));
+                yAxisMax = data1 > data2 ? data1 : data2;
 
-                this.$refs.chart.setOption({
-                    xAxis: [{data: xAxisData}],
-                    series: [{data: data1}, {data: data2}]
-                })
+                this.rxBytes = rxBytes;
+                this.txBytes = txBytes;
+
+                return {xAxisData, yAxisMax, data1, data2}
             }
         }
     }
